a403c42ca762804f3e29236b3e28be7f72c572f3
Tsukuru Tanimichi <info+git@ttanimichi.com>
2018-02-26 21:02:15 +0900

    Usage with Redux

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
   def show
-    render component: 'Post', props: { id: params[:id], body: 'foo bar' }, prerender: true
+    render component: 'Post', props: { post: { id: params[:id], body: 'foo bar' } }, prerender: true
   end
 end
--- app/javascript/components/Post.js
+++ app/javascript/components/Post.js
@@ -1,5 +1,37 @@
 import React from "react"
+import { Provider, connect } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+
+function post(state = null, action) {
+  return state;
+}
+
+const reducer = combineReducers({ post });
+
+function PostComponent(props) {
+  const { id, body } = props;
+
+  return (
+    <div>
+      <h1>Post</h1>
+      <div>{id}</div>
+      <div>{body}</div>
+    </div>
+  );
+}
+
+function mapStateToProps(state) {
+  return state.post;
+}
+
+const PostContainer = connect(mapStateToProps)(PostComponent);
 
 export default function Post(props) {
-  return <div>Post: {props.body}</div>;
+  const store = createStore(reducer, props);
+
+  return (
+    <Provider store={store}>
+      <PostContainer />
+    </Provider>
+  );
 }
